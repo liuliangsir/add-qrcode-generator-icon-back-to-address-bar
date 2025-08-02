5d343db13645c1cbad88e876a86eda878a327d13
liuliangsir <liuliang@w3ctech.com>
2025-08-02 23:14:45 +0800

    sss

---
+++ wxt.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from 'wxt'
+
+// See https://wxt.dev/api/config.html
+export default defineConfig({
+  manifest: {
+    permissions: ['activeTab'],
+    action: { default_popup: 'popup/index.html' },
+    name: 'Add QR Code Generator Icon Back To Address Bar',
+    description:
+      'A Extension To Add QR Code Generator Icon Back To Address Bar',
+    web_accessible_resources: [
+      {
+        resources: ['wasm/*'],
+        matches: ['<all_urls>']
+      }
+    ],
+    content_security_policy: {
+      extension_pages:
+        "script-src 'self' 'wasm-unsafe-eval'; object-src 'self';"
+    }
+  },
+  modules: ['@wxt-dev/module-react'],
+  entrypointsDir: 'src',
+  webExt: { disabled: true },
+  vite: () => ({
+    server: {
+      fs: {
+        allow: ['..']
+      }
+    },
+    assetsInclude: ['**/*.wasm']
+  }),
+  zip: {
+    artifactTemplate: '{{name}}-{{packageVersion}}-{{browser}}.zip',
+    sourcesTemplate: '{{name}}-{{packageVersion}}-{{browser}}-sources.zip'
+  }
+})
